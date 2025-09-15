156ba77d3d9e9e03dc6bab9650511322307722ef
Rasmuso23 <olausson.rasmus@gmail.com>
2025-09-15 18:45:11 +0200

    Added Pressable

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "expo-router";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, Pressable } from "react-native";
 
 export default function Index() {
     const router = useRouter();
@@ -7,7 +7,12 @@ export default function Index() {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>♠️Blackjack♠️</Text>
-            <Button title="Starta spelet" onPress={() => router.push("/game")} />
+            <Pressable style={styles.navButton} onPress={() => router.push("/game")}>
+                <Text style={styles.buttonText}>Start</Text>
+            </Pressable>
+                <Pressable style={styles.navButton}onPress={() => router.push("/")}>
+                    <Text style={styles.buttonText}>Stats</Text>
+                </Pressable>
         </View> 
     );
 }
@@ -16,11 +21,25 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "gray"
+        justifyContent: "center"
     },
     title: {
         fontSize: 48,
         fontWeight: "bold",
+    },
+    navButton: {
+        backgroundColor: "gray",
+        padding: 10,
+        width: 200,
+        height: 50,
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 10
+    },
+    buttonText: {
+        color: "white",
+        fontSize: 24,
+        fontWeight: "bold"
     }
 })
