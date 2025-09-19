238718f42c42b02398293f19b7f6e5e678d892ba
Rasmuso23 <olausson.rasmus@gmail.com>
2025-09-19 16:35:47 +0200

    .

--- src/components/deck.tsx
+++ src/components/deck.tsx
@@ -5,7 +5,7 @@ export type Card = {
 };
 
 export function createDeck(): Card[] {
-    const suits = ["♠️", "♥️", "♦️", "♣️"];
+    const suits = ["spade", "heart", "diamond", "club"];
     const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
     const deck: Card[] = [];
