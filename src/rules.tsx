69c539edd9371c6ce77297e29a4fa700a5042a0d
Rasmuso23 <olausson.rasmus@gmail.com>
2025-09-19 12:55:03 +0200

    Winner function

---
+++ src/rules.tsx
@@ -0,0 +1,24 @@
+import { getHandValue } from "./components/deck";
+import { useGame } from "./context/GameContext";
+import { Text } from "react-native";
+
+
+export function Winner() {
+    const { playerHand, dealerHand } = useGame();
+
+    const p = getHandValue(playerHand);
+    const d = getHandValue(dealerHand);
+
+    let message = " ";
+    if (p > d) {
+        message = "You win!";
+    }
+    if ( p === d && p !== 0) {
+        message = "Tie!"
+    }
+    else if (d > p) {
+        message = "Dealer win!";
+    }
+
+    return <Text>{message}</Text>;
+}
