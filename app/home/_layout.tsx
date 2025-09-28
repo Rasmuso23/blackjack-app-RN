098cefc7313f214cad3382c4204e9af8b25e650f
Rasmuso23 <olausson.rasmus@gmail.com>
2025-09-28 15:45:34 +0200

    tabs style

--- app/home/_layout.tsx
+++ app/home/_layout.tsx
@@ -1,30 +1,39 @@
 import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
-
+import { StyleSheet } from 'react-native';
 export default function HomeLayout() {
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
         tabBarShowLabel: false,
+        tabBarActiveTintColor: '#FFD700',
+        tabBarInactiveTintColor: 'white',
+
+        tabBarStyle: {
+          backgroundColor: 'black',
+          borderTopWidth: 0,
+          height: 100,
+          paddingTop: 10,
+        },
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
-          tabBarIcon: () => <Ionicons name="home" size={24} color="black" />,
+          tabBarIcon: ({ color }) => <Ionicons name="home" size={32} color={color} />,
         }}
       />
       <Tabs.Screen
         name="stats"
         options={{
-          tabBarIcon: () => <Ionicons name="bar-chart-sharp" size={24} color="black" />,
+          tabBarIcon: ({ color }) => <Ionicons name="bar-chart-sharp" size={32} color={color} />,
         }}
       />
       <Tabs.Screen
         name="wallet"
         options={{
-          tabBarIcon: () => <Ionicons name="wallet-sharp" size={24} color="black" />,
+          tabBarIcon: ({ color }) => <Ionicons name="wallet-sharp" size={32} color={color} />,
         }}
       />
     </Tabs>
